8a762705bd54cb803e45496fdf812c07eb64f271
Paulo Patto <paulopatto@gmail.com>
2015-02-09 19:45:35 -0200

    [development] remove ()

--- spec/footstats/resource_spec.rb
+++ spec/footstats/resource_spec.rb
@@ -10,11 +10,11 @@ describe Footstats::Resource do
     end
 
     it "adds given attribute a reader" do
-      expect(described_class.new).to respond_to :test
+      expect(described_class.new).to be_respond_to :test
     end
 
     it "adds given attribute a writer" do
-      expect(described_class.new).to respond_to :test=
+      expect(described_class.new).to be_respond_to :test=
     end
   end
 
@@ -28,11 +28,11 @@ describe Footstats::Resource do
       end
 
       it "transforms hash key Name in attr name" do
-        expect(resource).to be_respond_to(:name)
+        expect(resource).to be_respond_to :name
       end
 
       it "puts value of key Name in attr name" do
-        expect(resource.name).to eq(name)
+        expect(resource.name).to eq name
       end
     end
   end
