e935043e1a47783b28eb93fefae03b0ec4bc2d5d
Paulo Patto <paulopatto@gmail.com>
2015-03-05 19:55:33 -0300

    Atendendo modelo de dados do Caio Torres

--- lib/footstats/api/racing/gp.rb
+++ lib/footstats/api/racing/gp.rb
@@ -4,15 +4,15 @@ module Footstats
     module Racing
       class GP < Footstats::Resource
         field :id, :Id
-        field :track_name, :AutodromoName
-        field :track_nickname, :AutodromoApelido
-        field :distance, :DisatnciaPista
+        field :name, :AutodromoName
+        field :nickname, :AutodromoApelido
+        # field :distance, :DisatnciaPista
         field :locality, :Local
         field :date, :DataHoraBrasil
-        field :local_date, :DataHoraLocal
-        field :laps, :QtdVoltas
+        # field :local_date, :DataHoraLocal
+        field :total_laps, :QtdVoltas
         field :status, :Status
-        field :atual_lap, :VoltaAtual
+        # field :atual_lap, :VoltaAtual
         field :active, :Ativo
         field :city, :Cidade
         field :country, :Pais
