78c0174d90c9d02cbc9f9edba7733e1aa507e4fc
Paulo Patto <paulopatto@gmail.com>
2015-03-06 16:03:36 -0300

    Atendendo modelo de dados do Caio Torres

--- lib/footstats/api/racing/driver.rb
+++ lib/footstats/api/racing/driver.rb
@@ -6,17 +6,19 @@ module Footstats
       class Driver < Footstats::Resource
         field :id,                    :Id
         field :name,                  :Nome
-        field :brith,                 :Nascimento
-        field :points,                :PontosConquistados
-        field :podiums,               :Podios
-        field :first_gp,              :PrimeiroGP
+        # field :brith,                 :Nascimento
+        # field :points,                :PontosConquistados
+        # field :podiums,               :Podios
+        # field :first_gp,              :PrimeiroGP
         field :nickname,              :Apelido
-        field :brith_place,           :PaisOrigem
+        field :acronym,               :SiglaNome
+        # field :brith_place,           :PaisOrigem
         field :nationality,           :Nacionalidade
-        field :fastest_laps,          :VoltasRapidas
-        field :word_championships,    :Titulos
-        field :highest_grid_position, :Vitorias
-        field :highest_race_position, :PolesPositions
+        field :country_acronym,       :SiglaPais
+        # field :fastest_laps,          :VoltasRapidas
+        # field :word_championships,    :Titulos
+        # field :highest_grid_position, :Vitorias
+        # field :highest_race_position, :PolesPositions
       end
     end
   end
--- lib/footstats/api/racing/team.rb
+++ lib/footstats/api/racing/team.rb
@@ -4,6 +4,7 @@ class Footstats::Api::Racing::Team < Footstats::Resource
   field :id,       :Id
   field :name,     :Nome
   field :nickname, :Apelido
-  field :site,     :Site
-  field :country,  :SiglaPais
+  # field :site,     :Site
+  field :nationality, :Nacionalidade
+  field :country_acronym,  :SiglaPais
 end
