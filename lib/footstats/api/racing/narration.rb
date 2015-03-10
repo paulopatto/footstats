6631d0584f9056b620c1ad83a6e486197a492751
Paulo Patto <paulopatto@gmail.com>
2015-03-10 17:38:00 -0300

    Fix status

--- lib/footstats/api/racing/narration.rb
+++ lib/footstats/api/racing/narration.rb
@@ -2,7 +2,7 @@ require 'footstats/resource'
 
 class Footstats::Api::Racing::Narration < Footstats::Resource
   field :id, :Id
-  field :status, :status
+  field :status, :Status
   field :current_lap, :VoltaAtual
   field :datetime, :DataHora
   field :comment, :Comentario
